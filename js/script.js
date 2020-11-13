b72263a8f9a8179f68ce8d3e621c7d2b88a3622b
Szymon Krolikowski <krolikowski.szymon@outlook.com>
2020-11-13 14:38:13 +0100

    Add new function showButtonUp

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const navigationButton = document.querySelector(".button--navigationShow");
 const navigation = document.querySelector(".navigation");
 const navigationLinks = document.querySelectorAll(".navigation__link");
+const buttonUp = document.querySelector(".buttonUp")
+
+const showButtonUp = () => {
+    if(window.scrollY > 400) {
+        buttonUp.style.opacity = 1
+    } else {
+        buttonUp.style.opacity = 0
+    }
+};
 
 const toggleNavigation = () => {
     navigation.classList.toggle("navigation__active");
@@ -15,3 +24,4 @@ const toggleNavigation = () => {
 };
 
 navigationButton.addEventListener("click", toggleNavigation);
+window.addEventListener("scroll", showButtonUp)
