028e2bcd9445c30257aea73af3f943fe250f8d2c
Szymon Krolikowski <krolikowski.szymon@outlook.com>
2020-11-13 14:46:49 +0100

    Add functions showNextSlide and showPreviousSlide

--- js/script.js
+++ js/script.js
@@ -1,27 +1,67 @@
-const navigationButton = document.querySelector(".button--navigationShow");
-const navigation = document.querySelector(".navigation");
-const navigationLinks = document.querySelectorAll(".navigation__link");
-const buttonUp = document.querySelector(".buttonUp")
-
-const showButtonUp = () => {
-    if(window.scrollY > 400) {
-        buttonUp.style.opacity = 1
-    } else {
-        buttonUp.style.opacity = 0
-    }
-};
-
-const toggleNavigation = () => {
-    navigation.classList.toggle("navigation__active");
-
-    if(window.innerWidth <= 1024) {
-        navigationLinks.forEach(link => {
-            link.addEventListener("click", () => {
-                navigation.classList.remove("navigation__active");
-            });
-        });
+{
+    const navigationButton = document.querySelector(".button--navigationShow");
+    const navigation = document.querySelector(".navigation");
+    const navigationLinks = document.querySelectorAll(".navigation__link");
+    const buttonUp = document.querySelector(".buttonUp")
+    
+    const showButtonUp = () => {
+        if(window.scrollY > 400) {
+            buttonUp.style.opacity = 1
+        } else {
+            buttonUp.style.opacity = 0
+        }
     };
-};
+    
+    const toggleNavigation = () => {
+        navigation.classList.toggle("navigation__active");
+    
+        if(window.innerWidth <= 1024) {
+            navigationLinks.forEach(link => {
+                link.addEventListener("click", () => {
+                    navigation.classList.remove("navigation__active");
+                });
+            });
+        };
+    };
+    
+    navigationButton.addEventListener("click", toggleNavigation);
+    window.addEventListener("scroll", showButtonUp)
+}
 
-navigationButton.addEventListener("click", toggleNavigation);
-window.addEventListener("scroll", showButtonUp)
+{
+    const buttonNext = document.querySelector(".slider__button--next");
+    const buttonPrevious = document.querySelector(".slider__button--previous");
+    const slide = document.querySelectorAll(".slide__image");
+
+    let arrSlide = [];
+    let i = 0;
+    let index = 100;
+
+    slide.forEach(image => {
+        arrSlide.push(image);
+    });
+
+    const showNextSlide = () => {
+        i++;
+        if(i === 3) {
+            i = 0;
+        };
+
+        index++;
+        arrSlide[i].style.zIndex = index;
+    };
+
+    const showPreviousSlide = () => {
+        i--;
+        if(i === -1) {
+            i = 2;
+        };
+
+        index++;
+        arrSlide[i].style.zIndex = index;
+    }
+
+    buttonNext.addEventListener("click", showNextSlide);
+    buttonPrevious.addEventListener("click", showPreviousSlide);
+}
+
