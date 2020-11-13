959617f745ec5cbc30fd0c8948ec96b92c406070
Szymon Krolikowski <krolikowski.szymon@outlook.com>
2020-11-13 12:03:57 +0100

    Add toggleNavigation function

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ const navigation = document.querySelector(".navigation");
 const navigationLinks = document.querySelectorAll(".navigation__link");
 
 const toggleNavigation = () => {
-    navigation.classList.toggle("navigation__active")
+    navigation.classList.toggle("navigation__active");
 
     if(window.innerWidth <= 1024) {
         navigationLinks.forEach(link => {
             link.addEventListener("click", () => {
-                navigation.classList.remove("navigation__active")
-            })
-        })
-    }
-}
+                navigation.classList.remove("navigation__active");
+            });
+        });
+    };
+};
 
 navigationButton.addEventListener("click", toggleNavigation);
