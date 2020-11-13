4f0eaa83d242dcef31657ac91238e65eb875bda0
Szymon Krolikowski <krolikowski.szymon@outlook.com>
2020-11-13 12:03:24 +0100

    Remove data-aos-offset

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const navigationButton = document.querySelector(".button--navigationShow");
 const navigation = document.querySelector(".navigation");
+const navigationLinks = document.querySelectorAll(".navigation__link");
 
 const toggleNavigation = () => {
     navigation.classList.toggle("navigation__active")
+
+    if(window.innerWidth <= 1024) {
+        navigationLinks.forEach(link => {
+            link.addEventListener("click", () => {
+                navigation.classList.remove("navigation__active")
+            })
+        })
+    }
 }
 
 navigationButton.addEventListener("click", toggleNavigation);
